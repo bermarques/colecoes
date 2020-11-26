9398ba6c246f7f36f008b420d77d51031bcd236e
Bernardo Marques <bernardohmarques@gmail.com>
2020-11-26 10:30:20 -0300

    add animation to cards

--- src/components/charactersCard/index.jsx
+++ src/components/charactersCard/index.jsx
@@ -1,6 +1,6 @@
 import { Card } from "antd";
 import { Body } from "./style";
-
+import { motion } from "framer-motion";
 const { Meta } = Card;
 
 const CharactersCard = ({ arr }) => {
@@ -8,16 +8,21 @@ const CharactersCard = ({ arr }) => {
     <Body>
       {arr.map((card, idx) => {
         return (
-          <Card
-            key={idx}
-            hoverable
-            style={{ width: 240 }}
-            cover={
-              <img alt={"Imagem do personagem " + card.name} src={card.image} />
-            }
-          >
-            <Meta title={card.name} />
-          </Card>
+          <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
+            <Card
+              key={idx}
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt={"Imagem do personagem " + card.name}
+                  src={card.image}
+                />
+              }
+            >
+              <Meta title={card.name} />
+            </Card>
+          </motion.button>
         );
       })}
     </Body>
